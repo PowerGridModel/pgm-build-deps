e15b0d1cc12ff79a2f84f3b2a401a68f0cc63fdb
Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>
2026-05-11 13:49:51 +0200

    add test cmake
    Signed-off-by: Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>

---
+++ test_cmake/main.cpp
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+// main cpp file
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+
+// test all headers
+#include <doctest/doctest.h>
+#include <Eigen/Dense>
+#include <msgpack.hpp>
+#include <boost/iterator/counting_iterator.hpp>
+#include <CLI/CLI.hpp>
+#include <nlohmann/json.hpp>
